62e09fc57059a669cb3d25a99c8cf2518f6f624e
aop225 <aop22@outlook.com>
2023-10-05 19:13:03 -0400

    Commiting Navigation part

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/home";
+import Products from "./Products/Products";
 
 function App() {
   const openMenu = () => {
@@ -9,12 +12,11 @@ function App() {
   const closeMenu = () => {
     document.querySelector(".siderbar")?.classList.remove("open");
   }
-  return (
-    <div className="grid-container">
-    <head>
+    return (
+    <Router>
+        <div className="grid-container" />
+        <link rel="stylesheet" href="style.css" />      
         <title>Project Vino</title>
-    </head>
-    <link rel="stylesheet" href="style.css"></link>npm
     <body>
         <div>
             <header className="header">
@@ -23,8 +25,8 @@ function App() {
                     <a href="index.html">Project Vino</a>
                 </div>
                 <div className="header-links">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign In</a>
+                        <Link to="/">Home</Link>
+                        <Link to="/catalog">Catalog</Link>
                 </div>
             </header>
             <aside className="sidebar">
@@ -41,83 +43,17 @@ function App() {
                 </ul>
             </aside>
             <main>
-                <div className="content">
-                    <ul className="products">
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/catalog" element={<Products/>} />            
+                </Routes>
             </main>
             <footer>
                 &copy; 2023 Project Vino
             </footer>
         </div>
-    </body>
-</div>
+    </body>   
+    </Router>
   );
 }
 
